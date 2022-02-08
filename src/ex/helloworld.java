755d15b8dc0e80353f7c3a9e57539ecf611b09e8
annjaewuk <annjaewuk@gmail.com>
2022-02-08 13:36:04 +0900

    intital

---
+++ src/ex/helloworld.java
@@ -0,0 +1,14 @@
+package ex;
+
+// 자바기초 및 파이썬 차이점 정리
+
+public class helloworld { // 클래스 기반 객체지향 언어
+	
+    public static void main(String[] args) { //  함수 기본형식
+        System.out.println("Hello world");
+        System.out.println('우'); //자바는 한 글자는 작은따옴표
+        System.out.println("리엘소프트"); //자바는 문자열은 큰따옴표
+        /* 범위 주석 */
+    }
+
+}
